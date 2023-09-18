46c7def469dc1c2ce5ded2ed0b2078f658ca57c7
Andrey_Yudakov <viewsoul91@gmail.com>
2023-09-18 20:23:19 +0300

    Do #step2

--- gendiff/scripts/gendiff1.py
+++ gendiff/scripts/gendiff1.py
@@ -7,7 +7,7 @@ def say_welcome():
 
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
 
 
