e502898ccd8fac25a551c53ec9de1782ad4a8fc6
Andrey_Yudakov <viewsoul91@gmail.com>
2023-09-20 20:22:06 +0300

    Add test-coverage

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,8 +1,25 @@
+import os
+
+import pytest
+
 from gendiff import generate_diff
 
 
-def test_generate_diff():
-    assert generate_diff("./file1.json",
-                         "./file2.json") == ("{\n  - follow: false\n    host: hexlet.io\n "
-                                             " - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout"
-                                             ": 20\n  + verbose: true\n}")
+@pytest.fixture
+def file1():
+    return os.path.abspath("tests/fixtures/file1.json")
+
+
+@pytest.fixture
+def file2():
+    return os.path.abspath("tests/fixtures/file2.json")
+
+
+@pytest.fixture
+def expected():
+    with open(os.path.abspath("tests/fixtures/json_result")) as file:
+        return file.read()
+
+
+def test_generate_diff(file1, file2, expected):
+    assert generate_diff(file1, file2) == expected
