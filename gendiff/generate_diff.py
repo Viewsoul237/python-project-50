520e75d2b24ecfba1bd13830d2c3a0d29f9bdf15
Andrey_Yudakov <viewsoul91@gmail.com>
2023-09-21 22:55:07 +0300

    Fix linter warnings #2

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,9 +4,11 @@ import yaml
 
 
 def generate_diff(file_path1, file_path2):
-    file1, file2 = define_format(file_path1, file_path2)
+    file1 = define_format(file_path1)
+    file2 = define_format(file_path2)
     result = file1 | file2
     final_result = ["{"]
+
     for key in sorted(result.keys()):
         if file1.get(key) == file2.get(key):
             final_result.append(f"    {key}: {file1[key]}")
@@ -21,16 +23,12 @@ def generate_diff(file_path1, file_path2):
     return '\n'.join(final_result)
 
 
-def define_format(file_path_one, file_path_two):
-    with open(file_path_one) as file1, open(file_path_two) as file2:
-        if file_path_one.split('.')[-1] == "json":
-            file1 = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
-                     json.load(file1).items()}
-            file2 = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
-                     json.load(file2).items()}
+def define_format(file_path):
+    with open(file_path) as file:
+        if file_path.endswith("json"):
+            file = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
+                    json.load(file).items()}
         else:
-            file1 = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
-                     yaml.load(file1, Loader=yaml.Loader).items()}
-            file2 = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
-                     yaml.load(file2, Loader=yaml.Loader).items()}
-    return file1, file2
+            file = {k: str(v).lower() if isinstance(v, bool) else v for k, v in
+                    yaml.load(file, Loader=yaml.Loader).items()}
+    return file
