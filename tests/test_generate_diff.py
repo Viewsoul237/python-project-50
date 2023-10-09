f6e0c88063942461fcd1a2b234888e14c3a8c00d
Andrey_Yudakov <viewsoul91@gmail.com>
2023-10-09 15:13:44 +0300

    Fix some issues #4

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -7,50 +7,50 @@ from gendiff import generate_diff
     [
         # №1 Testing flat files (json/yml)
         (
-                "tests/fixtures/file1.json",
-                "tests/fixtures/file2.yml",
-                "stylish",
-                "tests/fixtures/result",
+            "tests/fixtures/file1.json",
+            "tests/fixtures/file2.yml",
+            "stylish",
+            "tests/fixtures/result",
         ),
 
         # №2 Testing flat files (yaml/json)
         (
-                "tests/fixtures/file1.yaml",
-                "tests/fixtures/file2.json",
-                "stylish",
-                "tests/fixtures/result",
+            "tests/fixtures/file1.yaml",
+            "tests/fixtures/file2.json",
+            "stylish",
+            "tests/fixtures/result",
         ),
 
         # №3 Testing nested files (json/json)
         (
-                "tests/fixtures/nested1.json",
-                "tests/fixtures/nested2.json",
-                "stylish",
-                "tests/fixtures/nested_result",
+            "tests/fixtures/nested1.json",
+            "tests/fixtures/nested2.json",
+            "stylish",
+            "tests/fixtures/nested_result",
         ),
 
         # №4 Testing nested files (yml/json)
         (
-                "tests/fixtures/nested1.yml",
-                "tests/fixtures/nested2.json",
-                "stylish",
-                "tests/fixtures/nested_result",
+            "tests/fixtures/nested1.yml",
+            "tests/fixtures/nested2.json",
+            "stylish",
+            "tests/fixtures/nested_result",
         ),
 
         # №5 Testing nested files (json/yaml) # plain output
         (
-                "tests/fixtures/nested1.json",
-                "tests/fixtures/nested2.yaml",
-                "plain",
-                "tests/fixtures/flat_result",
+            "tests/fixtures/nested1.json",
+            "tests/fixtures/nested2.yaml",
+            "plain",
+            "tests/fixtures/flat_result",
         ),
 
         # №6 Testing nested files (yml/yaml) # json output
         (
-                "tests/fixtures/nested1.yml",
-                "tests/fixtures/nested2.yaml",
-                "json",
-                "tests/fixtures/json_result",
+            "tests/fixtures/nested1.yml",
+            "tests/fixtures/nested2.yaml",
+            "json",
+            "tests/fixtures/json_result",
         ),
     ]
 )
